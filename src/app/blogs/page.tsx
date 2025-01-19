88c9cf9434b918929515ffa83d8a2bb4d78aa553
Justin Korkie <justinjkorkie@gmail.com>
2025-01-20 01:41:43 +0200

    chore(src): update fonts & adjust alignment on blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,8 +7,11 @@ import { ArrowLeftCircle } from "react-feather";
 const BlogsComponent: React.FC = () => {
 
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-orange-200 to-black-600 p-4">
-      <Link className='markdown ' style={{}} href={'/'}><ArrowLeftCircle style={{ fontSize: 20, color: "black" }} /></Link>
+    <div className="flex flex-col min-h-screen bg-gradient-to-r from-orange-200 to-black-600 p-4 items-center">
+
+      <div className="w-full md:w-1/2 lg:w-1/2">
+        <Link className='markdown ' style={{}} href={'/'}><ArrowLeftCircle style={{ fontSize: 20, color: "black" }} /></Link>
+      </div>
       <Blogs />
     </div>
   );
--- src/components/Blogs/index.tsx
+++ src/components/Blogs/index.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import { parseMdString } from "@/util/parse-md-to-heading"
 import { useEffect, useState } from 'react';
 import { Book } from 'react-feather';
-import { Caveat } from 'next/font/google';
+import { Caveat, Puppies_Play } from 'next/font/google';
 import { motion } from 'framer-motion';
 
 const caveat = Caveat({ weight: "400", subsets: ["latin"] })
+const puppies = Puppies_Play({ weight: "400", subsets: ["latin"] })
 
 const Blogs: React.FC = () => {
   const [blogPaths, setBlogPaths] = useState<string[]>([])
@@ -19,16 +20,16 @@ const Blogs: React.FC = () => {
     getBlogNames()
   }, []);
   return (
-    <div className="text-center mt-8 flex flex-col  ">
+    <div className="text-center mt-8 flex flex-col w-3/4 items-center">
 
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <h2 className={`text-4xl font-bold mb-4 text-gray-200 ${caveat.className}`}>Blogs</h2>
+        <h2 className={`${puppies.className} text-6xl font-bold mb-4 text-gray-200`}>Blogs</h2>
       </motion.div>
-      <ul className='flex flex-col text-xl p-5 gap-4 text-gray-500'>
+      <ul className='flex flex-col text-xl p-5 gap-4 text-gray-500 w-full'>
         {blogPaths.map((path, index) => {
           const parsedMd = parseMdString(path);
           return (
@@ -40,10 +41,10 @@ const Blogs: React.FC = () => {
               whileTap={{ scale: 0.95 }}
               key={path}
             >
-              <li className="w-full">
-                <div className=" shadow-md w-full md:w-1/4 lg:w-1/4 p-2">
+              <li className="w-full flex items-center justify-center">
+                <div className={`${caveat.className} shadow-md w-full md:w-1/4 lg:w-1/4 p-2`}>
                   <Link href={`blog/${path}`} className={"text-left "} >
-                    <p className="text-3xl flex flex-row items-center gap-2"><Book />{parsedMd.heading ?? ""}</p>
+                    <p className="text-3xl flex flex-row items-center gap-2 text-gray-700"><Book />{parsedMd.heading ?? ""}</p>
                     <p className="text-sm"><em>{parsedMd.date ?? ""}</em></p>
 
                   </Link>
